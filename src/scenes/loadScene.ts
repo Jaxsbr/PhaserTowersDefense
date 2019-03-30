10ec1dc4eb4eae4a7d6b14577f9849340e4c906b
Jacobus <jbrink386@gmail.com>
2019-03-30 21:26:31 +0200

    relative path change

--- src/scenes/loadScene.ts
+++ src/scenes/loadScene.ts
@@ -6,27 +6,27 @@ export class LoadScene extends Phaser.Scene {
   }
 
   preload(): void {
-    this.load.image('background', '../assets/background.png');
+    this.load.image('background', '/assets/background.png');
 
     this.load.spritesheet('tower_plain', '../assets/tower_plain.png', {
       frameWidth: 32,
       frameHeight: 32,
     });
 
-    this.load.spritesheet('tower_slow', '../assets/tower_slow.png', {
+    this.load.spritesheet('tower_slow', '/assets/tower_slow.png', {
       frameWidth: 32,
       frameHeight: 32,
     });
 
-    this.load.spritesheet('ork', '../assets/ork_spritesheet.png', {
+    this.load.spritesheet('ork', '/assets/ork_spritesheet.png', {
       frameWidth: 96,
       frameHeight: 96,
     });
-    this.load.spritesheet('squid', '../assets/squid_spritesheet.png', {
+    this.load.spritesheet('squid', '/assets/squid_spritesheet.png', {
       frameWidth: 64,
       frameHeight: 80,
     });
-    this.load.spritesheet('tiles', '../assets/tile_spritesheet.png', {
+    this.load.spritesheet('tiles', '/assets/tile_spritesheet.png', {
       frameWidth: 32,
       frameHeight: 32,
     });
